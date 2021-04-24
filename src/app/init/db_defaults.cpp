decd37db40b72ce6cac441039826e089ea432c5b
Ashwin Natarajan <ashwin.nat@gmail.com>
2021-04-24 15:07:16 +0530

    removed blacklist table

--- src/app/init/db_defaults.cpp
+++ src/app/init/db_defaults.cpp
@@ -8,15 +8,13 @@ using db_table_t = std::vector<db_col_info_t>;
 using db_schema_t = std::map<std::string,db_table_t>;
 
 static void _add_info_of_stats (db_schema_t &schema);
-static void _add_info_of_blacklist (db_schema_t &schema);
 
 void _get_expected_db_schema (db_schema_t &schema)
 {
     _add_info_of_stats (schema);
-    _add_info_of_blacklist (schema);
 }
 
-static void 
+static void
 _add_info_of_stats (
     db_schema_t &schema)
 {
@@ -24,18 +22,6 @@ _add_info_of_stats (
     db_table_t cols = {
         {"ipaddr", "TEXT", "PRIMARY KEY"},
         {"count", "INTEGER", "DEFAULT 0"},
-    };
-
-    schema.insert (std::pair<std::string,db_table_t> (table_name, cols));
-}
-
-static void 
-_add_info_of_blacklist (
-    db_schema_t &schema)
-{
-    const std::string table_name = "blacklist";
-    db_table_t cols = {
-        {"ipaddr", "TEXT", "PRIMARY KEY"},
     };
 
     schema.insert (std::pair<std::string,db_table_t> (table_name, cols));
